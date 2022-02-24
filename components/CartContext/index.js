7e77f077f6afade64f47a4b61204d1c1a3634d79
Anjnaeyulu143 <92676137+Anjnaeyulu143@users.noreply.github.com>
2022-02-24 07:57:36 +0530

    Update index.js

--- components/CartContext/index.js
+++ components/CartContext/index.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+import counterReducer from "../CounterReducer";
+import todoReducer from "../TodoReducer";
 
 export const Context = React.createContext();
 
 export const CardContext = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  const [counter, dispatch] = useReducer(counterReducer, {
+    count: 0,
+  });
+
+  const [todolist, tododispatch] = useReducer(todoReducer, {
+    todoList: ["Anjan"],
+  });
+
   return (
     <>
-      <Context.Provider value={{ cart, setCart }}>{children}</Context.Provider>
+      <Context.Provider
+        value={{ cart, counter, todolist, setCart, dispatch, tododispatch }}
+      >
+        {children}
+      </Context.Provider>
     </>
   );
 };
